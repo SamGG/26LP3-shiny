3d9e8afe1bc98a1dd7a123667a992efe0d97920e
SamGG <samuel.granjeaud@inserm.fr>
2026-01-21 15:19:22 +0100

    backend skeleton

---
+++ 1_backend.R
@@ -0,0 +1,20 @@
+generate_data <- function(
+    diff = 1,
+    n = 10,
+    s = 1000
+) {
+  #
+  res <- matrix()
+  # simulation 1st group
+  # simulation 2nd group
+  return(res)
+}
+
+plot_hist <- function(
+    mat
+) {
+  # plot 1st histogram
+  hist()
+  # plot 2nd histogram
+  hist(, add = TRUE)
+}
