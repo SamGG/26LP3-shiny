ccece315d0bfbe38c457515cc734f7d17f7e97a6
SamGG <samuel.granjeaud@inserm.fr>
2026-01-21 16:04:38 +0100

    update

--- 1_backend.R
+++ 1_backend.R
@@ -4,15 +4,26 @@ generate_data <- function(
     s = 1000
 ) {
   #
-  res <- matrix()
   # simulation 1st group
+  gr1 <- rnorm( n * s )
   # simulation 2nd group
+  gr2 <- rnorm( n * s, mean = diff )
+  # insert values into a matrix 2D
+  res <- matrix( c( gr1, gr2 ), nrow = 2 * n, ncol = s, byrow = TRUE)
+  # check heatmap
+  pheatmap::pheatmap(res, cluster_rows = FALSE, cluster_cols = FALSE)
   return(res)
 }
 
 plot_hist <- function(
     mat
 ) {
+  # compute n
+  n <- nrow( mat ) / 2
+  # calculate mean gr1
+  mat1 <- mat[ 1:n, ]
+  mean1 <- apply( mat1, 2, mean )
+  # calculate mean gr2
   # plot 1st histogram
   hist()
   # plot 2nd histogram
